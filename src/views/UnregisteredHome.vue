9ee082f156cd3e5ee293cfc09ef6575c55ae7fde
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-10-27 13:19:18 +0300

    Changed name to sidebar

--- src/views/UnregisteredHome.vue
+++ src/views/UnregisteredHome.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
-import NavBarUnregistered from "@/components/NavBarUnregistered.vue";
+import SideBarUnregistered from "@/components/SideBarUnregistered.vue";
+import TopNavbar from "@/components/TopNavbar.vue";
 </script>
 
 
 <template>
   <div class="max-w-screen-2xl h-screen bg-grey-1 mx-auto flex ">
-    <NavBarUnregistered/>
+    <SideBarUnregistered/>
+    <div>
+      <TopNavbar/>
+    </div>
 
   </div>
 </template>
