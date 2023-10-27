882685df4ba8ba90f2d4c2686c9e73b3f2c8e97f
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-10-27 13:08:36 +0300

    Sidebar,unregisterd page

---
+++ src/components/NavBarUnregistered.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+  import SchoolLogo from "./icons/IconSchoolLogo.vue"
+  import SettingsLogo from "./icons/IconSettings.vue"
+  import LineIcon from "./icons/IconLine.vue"
+</script>
+
+<template>
+    <nav class="w-24 h-full bg-white rounded-r-3xl flex flex-col justify-between">
+      <div class="mx-auto mt-11">
+        <div class="ml-3.5">
+          <SchoolLogo/>
+        </div>
+        <div class="mx-auto mt-7">
+          <LineIcon/>
+        </div>
+      </div>
+
+      <div class="mx-auto mb-9">
+        <SettingsLogo/>
+      </div>
+
+    </nav>
+</template>
