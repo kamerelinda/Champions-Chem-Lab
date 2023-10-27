20ee19abf2f89edb127f9f1f1a6cfba7d40f5e43
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-10-27 13:07:59 +0300

    Unregistered Home view

---
+++ src/views/UnregisteredHome.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import NavBarUnregistered from "@/components/NavBarUnregistered.vue";
+</script>
+
+
+<template>
+  <div class="max-w-screen-2xl h-screen bg-grey-1 mx-auto flex ">
+    <NavBarUnregistered/>
+
+  </div>
+</template>
