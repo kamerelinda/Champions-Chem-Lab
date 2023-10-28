4e99ce6560c3ee068110fdea152e28299f56ed06
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-10-28 13:42:30 +0300

    Unregisterd Home

---
+++ src/components/Homepage1.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import TopNavbar from "@/components/TopNavbar.vue";
+import TrainingVideos from "@/components/TrainingVideos.vue";
+</script>
+
+<template>
+  <div class="w-full">
+  <TopNavbar/>
+  <TrainingVideos/>
+  </div>
+</template>
