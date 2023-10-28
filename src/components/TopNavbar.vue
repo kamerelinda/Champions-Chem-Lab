90c9873d285add618d568d753b9de8541bf1d3e1
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-10-28 11:45:46 +0300

    Top Navbar

---
+++ src/components/TopNavbar.vue
@@ -0,0 +1,15 @@
+<script setup  lang="ts">
+  import SearchBar from "@/components/SearchBar.vue";
+  import ToSignUp from "@/components/ToSignUp.vue";
+</script>
+
+<template>
+  <div class="w-full h-24  flex justify-between">
+    <div class="mx-auto my-auto">
+      <SearchBar/>
+    </div>
+    <div class="my-auto">
+      <ToSignUp/>
+    </div>
+  </div>
+</template>
