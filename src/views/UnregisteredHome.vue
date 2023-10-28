e8043ece444189ea91326e4efefe308620ab2b99
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-10-28 13:42:50 +0300

    Add modal login

--- src/views/UnregisteredHome.vue
+++ src/views/UnregisteredHome.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
 import SideBarUnregistered from "@/components/SideBarUnregistered.vue";
-import TopNavbar from "@/components/TopNavbar.vue";
+import Homepage1 from "@/components/Homepage1.vue";
+
+import {ref} from 'vue';
+
+const showModal = ref(true)
 </script>
 
 
 <template>
+  <div v-show="showModal" class="absolute w-full h-full bg-GreyOverlay z-10 flex justify-center items-center">
+    <div class="modal">
+      <textarea name="note" id="note" cols="30" rows="10"></textarea>
+      <button >Add Note</button>
+      <button @click="showModal = false" class="close">Close</button>
+    </div>
+  </div>
   <div class="max-w-screen-2xl flex mx-auto bg-grey-1">
     <SideBarUnregistered/>
-    <TopNavbar/>
+    <Homepage1/>
   </div>
 </template>
