9bfa92145f8f811d2a6de633571b731143ba1ef3
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-11-03 12:43:21 +0300

    Registered sidebar

---
+++ src/components/RegisteredNav.vue
@@ -0,0 +1,19 @@
+<script setup>
+import IconHome from "@/components/icons/IconHome.vue";
+import IconVirtualGames from "@/components/icons/IconVirtualGames.vue";
+import IconPuzzleGames from "@/components/icons/IconPuzzleGames.vue";
+import IconTrainingVideo from "@/components/icons/IconTrainingVideo.vue";
+import IconCommunities from "@/components/icons/IconCommunities.vue";
+import IconHomework from "@/components/icons/IconHomework.vue";
+</script>
+
+<template>
+  <div>
+      <icon-home class="mx-auto mt-10 mb-16"/>
+      <icon-virtual-games class="mx-auto mb-16"/>
+      <icon-puzzle-games class="mx-auto mb-16"/>
+      <icon-training-video class="mx-auto mb-16"/>
+      <icon-communities class="mx-auto mb-16"/>
+      <icon-homework class="mx-auto mb-16"/>
+  </div>
+</template>
--- src/components/SideBarUnregistered.vue
+++ src/components/SideBarUnregistered.vue
@@ -2,21 +2,39 @@
   import SchoolLogo from "./icons/IconSchoolLogo.vue"
   import SettingsLogo from "./icons/IconSettings.vue"
   import LineIcon from "./icons/IconLine.vue"
+  import RegisteredNav from "@/components/RegisteredNav.vue";
+  import IconLogout from "@/components/icons/IconLogout.vue";
+  import IconChampionsLab from "@/components/icons/IconChampionsLab.vue";
+
+  import {ref} from "vue";
+
+  const loggedin = ref(true)
 </script>
 
 <template>
     <nav class="w-24 h-screen bg-white rounded-r-3xl flex flex-col justify-between">
       <div class="mx-auto mt-11">
-        <div class="ml-3.5">
-          <SchoolLogo/>
+        <div>
+          <IconChampionsLab  v-if="loggedin" class="mx-auto"/>
+          <SchoolLogo v-else class="mx-auto"/>
         </div>
         <div class="mx-auto mt-7">
           <LineIcon/>
         </div>
+        <div>
+          <RegisteredNav v-show="loggedin"/>
+        </div>
       </div>
-      <div class="mx-auto mb-9">
-        <SettingsLogo/>
-      </div>
+<div>
+  <div v-if=loggedin>
+    <SettingsLogo class="mx-auto"/>
+    <icon-logout class=" mx-auto mb-9 mt-11"/>
+  </div>
+    <div v-else class="mb-9 ">
+      <SettingsLogo class="mx-auto"/>
+    </div>
 
+
+</div>
     </nav>
 </template>
