83399c3bd70b07eb792c23c5c456a4c880d66cc4
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-11-07 11:41:30 +0300

    Video carousel

---
+++ src/components/TrainingVideos.vue
@@ -0,0 +1,17 @@
+<template>
+  <p class=" ml-36 mt-10 font-manrope text-base font-bold">Get started with some experiments</p>
+
+  <div class="flex justify-center relative w-3/4 h-64 mx-auto cursor-pointer">
+    <div class="absolute z-50 inset-y-0 top-10">
+      <img src="@/assets/image%201.png" alt="">
+    </div>
+
+    <div class="bg-video1 shadow-box1 h-56 w-64 z-40 inset-y-0 absolute top-14 left-36"></div>
+
+    <div class="bg-video1 shadow-box1 h-56 w-64  z-40 inset-y-0 absolute top-14 right-36"></div>
+
+    <div class="bg-video2 h-52 w-52 z-30 inset-y-0 absolute top-16 left-12"></div>
+
+    <div class="bg-video2 h-52 w-52 z-30 inset-y-0 absolute top-16 right-12 "></div>
+  </div>
+</template>
