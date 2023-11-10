a798d49d520c3ad86c1bf6b390eda76bbddc8ee8
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-11-10 16:54:23 +0300

    added routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import UnregisteredHome from '../views/UnregisteredHome.vue'
+import HomeView from "@/views/HomeView.vue";
+import VirtualGamesView from "@/views/VirtualGamesView.vue";
+import AssignmentsView from "@/views/AssignmentsView.vue";
+import CommunitiesView from "@/views/CommunitiesView.vue";
+import VideoLessonsView from "@/views/VideoLessonsView.vue";
+import PuzzleGamesView from "@/views/PuzzleGamesView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +12,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home1',
-      component: UnregisteredHome
+      component: HomeView
     },
     {
       path: '/about',
@@ -16,7 +21,33 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    }
+    },
+    {
+      path: '/3dgames',
+      name: 'gaming',
+      component: VirtualGamesView
+    },
+    {
+      path: '/puzzlegames',
+      name: 'puzzles',
+      component: PuzzleGamesView
+    },
+    {
+      path: '/videolessons',
+      name: 'training videos',
+      component: VideoLessonsView
+    },
+    {
+      path: '/community',
+      name: 'community',
+      component: CommunitiesView
+    },
+    {
+      path: '/assignments',
+      name: 'homework',
+      component: AssignmentsView
+    },
+
   ]
 })
 
