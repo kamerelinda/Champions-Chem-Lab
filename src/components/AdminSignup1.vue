06a99eb6a3971b70ea8d824c53277fc9c5790789
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-11-23 13:35:54 +0300

    Admin sign up page 1

---
+++ src/components/AdminSignup1.vue
@@ -0,0 +1,115 @@
+<script setup lang="ts">
+
+import {Dialog, DialogPanel, TransitionChild, TransitionRoot} from "@headlessui/vue";
+import {EyeIcon} from "@heroicons/vue/24/outline";
+import {ref} from "vue";
+import IconSchoolLogo from "@/components/icons/IconSchoolLogo.vue";
+
+const open = ref(true)
+</script>
+
+<template>
+  <TransitionRoot as="template" :show="open">
+    <Dialog as="div" class="relative z-10" @close="open = false">
+      <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
+        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+      </TransitionChild>
+
+      <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+        <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+            <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-[520px] h-[909px] sm:px-6 sm:py-8">
+              <div class=" font-manrope md:mt-0 sm:max-w-md xl:p-0">
+                <div class="space-y-4 md:space-y-6">
+                  <div class="flex flex-col justify-center items-center text-center">
+                    <p class="text-Primary pb-2 overflow-hidden text-ellipsis whitespace-nowrap text-base font-extrabold">Champions Chem Lab</p>
+                    <IconSchoolLogo class="pb-2"/>
+                    <h1 class="text-xl pb-2 font-bold leading-tight tracking-tight text-Neutral/800 md:text-2xl dark:text-white">
+                      Admin Sign Up
+                    </h1>
+                    <p class="text-sm font-normal text-Neutral/500 ">We’ll get you up and running so you can verify your personal information and customize your account.</p>
+                  </div>
+                  <form class="space-y-4 md:space-y-6" action="#">
+<!--                         name section-->
+                    <div class="flex flex-row gap-x-7 sm:mx-auto">
+                      <div>
+                        <label for="firstName" class="block text-sm font-medium leading-6 text-gray-900">First Name</label>
+                        <div class="mt-2">
+                          <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Jane" required/>
+                        </div>
+                      </div>
+                      <div>
+                        <label for="lastName" class="block text-sm font-medium leading-6 text-gray-900">Last Name</label>
+                        <div class="mt-2">
+                          <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Doe" required/>
+                        </div>
+                      </div>
+                    </div>
+<!--                    end of name section -->
+<!--                    email section-->
+                    <div>
+                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email Address</label>
+                      <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="hello@example.com" required>
+                    </div>
+<!--                    end of email section-->
+<!--                    Password section-->
+                    <div>
+                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Create Password</label>
+                      <div class="relative">
+                      <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required >
+                      <div class="w-4 h-4 absolute inset-y-0 right-2 my-auto cursor-pointer">
+                        <EyeIcon />
+                      </div>
+                      </div>
+                    </div>
+                    <div>
+                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm Password</label>
+                      <div class="relative">
+                      <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                        <div class="w-4 h-4 absolute inset-y-0 right-2 my-auto cursor-pointer">
+                          <EyeIcon />
+                        </div>
+                      </div>
+                    </div>
+<!--                    end of password section-->
+<!--                    Remember me section-->
+                    <div class="flex items-center justify-between">
+                      <div class="flex items-start">
+                        <div class="flex items-center h-5">
+                          <input id="tick" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required>
+                        </div>
+                        <div class="ml-3 text-sm">
+                          <label for="remember" class="text-gray-500 dark:text-gray-300">Remember Me</label>
+                        </div>
+                      </div>
+                    </div>
+<!--                    end of Remember me section-->
+<!--                    How did you hear about us-->
+                    <div>
+                      <label for="reference" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">How did you hear about us? (optional)</label>
+                      <select class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <option value="">Friends&Family</option>
+                        <option value="">Social Media</option>
+                        <option value="">Other</option>
+                      </select>
+                    </div>
+<!--                    end of how did you hear about us-->
+<!--                    sign up button-->
+                    <button type="submit" class="w-full text-white bg-Primary hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 roundesd text-base font-semibold px-5 py-1.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign Up</button>
+                    <p class="text-sm font-normal text-Neutral/800 text-center dark:text-gray-400">
+                      Already have an account? <a href="#" class="font-bold text-sm text-Primary hover:underline dark:text-primary-500">Login here</a>
+                    </p>
+                  </form>
+                </div>
+              </div>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+</template>
+
+<style scoped>
+
+</style>
