e0a87ce60e06fe06536ee66ad19c76e2f26ea7a3
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-11-24 11:41:43 +0300

    Build classroom form

---
+++ src/components/AdminBuildClassRoom.vue
@@ -0,0 +1,97 @@
+<script setup lang="ts">
+
+import {Dialog, DialogPanel, TransitionChild, TransitionRoot} from "@headlessui/vue";
+import {ref} from "vue";
+import IconSchoolLogo from "@/components/icons/IconSchoolLogo.vue";
+import EnrollStudents from "@/components/EnrollStudents.vue";
+
+const open = ref(true)
+</script>
+
+<template>
+  <TransitionRoot as="template" :show="open">
+    <Dialog as="div" class="relative z-10" @close="open = false">
+      <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
+        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+      </TransitionChild>
+
+      <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+        <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+            <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-[520px] h-[950px] sm:px-6 sm:py-8">
+              <div class=" font-manrope md:mt-0 sm:max-w-md xl:p-0">
+                <div class="space-y-4 md:space-y-6">
+                  <div class="flex flex-col justify-center items-center text-center">
+                    <p class="text-Primary pb-2 overflow-hidden text-ellipsis whitespace-nowrap text-base font-extrabold">Champions Chem Lab</p>
+                    <IconSchoolLogo class="pb-2"/>
+                    <h1 class="text-xl pb-2 font-bold leading-tight tracking-tight text-Neutral/800 md:text-2xl dark:text-white">
+                      Build your Classroom
+                    </h1>
+                    <p class="text-sm font-normal text-Neutral/500 ">You will need a classroom in order to enroll your students to the platrform,</p>
+                  </div>
+                  <!--                  divider section -->
+                  <div class="flex flex-row">
+                    <n-divider dashed class="text-sm text-Neutral/800">
+                      <div class="border-2 rounded bg-Primary border-Primary h-6 w-6 p-1 flex justify-center items-center">1</div>
+                      <div class="pl-1">Sign Up</div>
+                    </n-divider>
+                    <n-divider dashed class="text-sm text-Neutral/800">
+                      <div class="border-2 rounded border-Primary bg-Primary h-6 w-6 p-1 flex justify-center items-center" >2</div>
+                      <div class="pl-1">Build Classroom</div>
+                    </n-divider>
+                    <n-divider dashed class="text-sm">
+                      <div class="border-2 rounded border-Primary  h-6 w-6 p-1 flex justify-center items-center" >3</div>
+                      <div class="pl-1 text-neutral-400">Activate Plan</div>
+                    </n-divider>
+                  </div>
+                  <!--                  end of divider section-->
+                  <form class="space-y-4 md:space-y-6" action="#">
+
+                    <!--                   Class Name section-->
+                    <div>
+                      <label for="class" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Class Name</label>
+                      <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                    </div>
+                    <!--                    end of Class Name section-->
+                    <!--                    Number of Student to enroll section-->
+                    <div>
+                      <label for="number" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Number of students to enroll</label>
+                      <div class="relative">
+                        <input type="number" placeholder="at least 1" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                      </div>
+                    </div>
+                    <!--                    end of number of students section-->
+                    <EnrollStudents/>
+                    <!--                    share-->
+
+                        <div>
+                          <div class="mt-2 max-w-xl text-sm text-gray-500">
+                            <p>or share link to join</p>
+                          </div>
+                          <form class="mt-5 sm:flex sm:items-center">
+                            <div class="w-full sm:max-w-xs">
+                              <label for="link" class="sr-only"></label>
+                              <input type="email" name="email" id="email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="https://www.sharemylink.com" />
+                            </div>
+                            <button type="submit" class="mt-3 inline-flex w-full items-center justify-center rounded-md  px-3 py-2 text-sm font-semibold text-Primary shadow-sm hover:bg-Primary hover:text-white sm:ml-3 sm:mt-0 sm:w-auto">Copy</button>
+                          </form>
+                        </div>
+
+
+                    <!--                    end of share-->
+                    <!--                    build classroom button-->
+                    <button type="submit" class="w-full text-white bg-Primary hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 roundesd text-base font-semibold px-5 py-1.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Build Classroom</button>
+                  </form>
+                </div>
+              </div>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/EnrollStudents.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+const posts = [
+  {
+    id: 1,
+    author: {
+      name: 'Michael Foster',
+      email: 'michael@gmail.com',
+      href: '#',
+      imageUrl:
+          'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+  },
+  {
+    id: 2,
+    author: {
+      name: 'Stella James',
+      email: 'stella@gmail.com',
+      href: '#',
+      imageUrl:
+          'https://images.unsplash.com/photo-1554151228-14d9def656e4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cGVyc29ufGVufDB8fDB8fHww',
+
+  },},
+  {
+    id: 3,
+    author: {
+      name: 'Mary keynes',
+      email: 'keynesmary@gmail.com',
+      href: '#',
+      imageUrl:
+          'https://plus.unsplash.com/premium_photo-1668989224643-6b79eaea2108?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D',
+
+    },},
+
+]
+</script>
+
+<template>
+  <p>Enroll students into your classroom</p>
+  <article v-for="post in posts" :key="post.id" class="flex max-w-xl flex-col items-start justify-between">
+  <div class="relative flex items-center gap-x-4">
+    <img :src="post.author.imageUrl" alt="" class="h-10 w-10 rounded-full bg-gray-50" />
+    <div class="text-sm leading-6">
+      <p class="font-semibold text-gray-900">
+        <a :href="post.author.href">
+          <span class="absolute inset-0" />
+          {{ post.author.name }}
+        </a>
+      </p>
+      <p class="text-gray-600">{{ post.author.email}}</p>
+    </div>
+  </div>
+  </article>
+</template>
+
+<style scoped>
+
+</style>
