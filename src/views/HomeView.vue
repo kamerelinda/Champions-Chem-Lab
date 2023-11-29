311b0934077965a111244d47c7fa22aacb9cb2ec
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-11-29 19:16:09 +0300

    Remove the sidebar on the view and inserts it into the homepages

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,17 @@
 <script setup lang="ts">
-import SideBar from "@/components/SideBarNavigation.vue";
 import Homepage1 from "@/components/Homepage1.vue";
 import Homepage2 from "@/components/Homepage2.vue";
 import {useLoadHomeStore} from "@/stores/loadHomepage";
 const loadHome = useLoadHomeStore()
+
+import {useSidebarOpen} from "@/stores/sidebarOpen";
+const openBar = useSidebarOpen()
 </script>
 
 
 <template>
-  <div class="sm:max-w-screen-2xl flex mx-auto bg-grey-1 h-[1000px]">
+  <div class="sm:max-w-screen-2xl flex mx-auto bg-grey-1 h-[1000px] relative">
 
-    <SideBar/>
     <Homepage2 v-if="loadHome.Home2"/>
     <Homepage1 v-else/>
   </div>
