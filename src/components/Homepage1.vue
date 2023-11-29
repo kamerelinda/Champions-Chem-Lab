10de4668d852bdc8cdb6a577540337caf9b1d1ff
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-11-29 19:21:09 +0300

    Insert unregistered sidebar version

--- src/components/Homepage1.vue
+++ src/components/Homepage1.vue
@@ -2,14 +2,13 @@
 import TopNavbar from "@/components/TopNavbar.vue";
 import TrainingVideos from "@/components/TrainingVideos.vue";
 import CommentSection from "@/components/CommentSection.vue";
-
-
-
+import SideBar1 from "@/components/SidebarUnregistered.vue";
 </script>
 
 <template>
-  <div class="w-full h-full relative">
-  <TopNavbar/>
+  <div class="w-full h-fit relative">
+    <SideBar1 class="absolute left-0  w-24 shadow-2xl h-full"/>
+    <TopNavbar/>
   <TrainingVideos/>
   <CommentSection />
   </div>
