f9c26154ff20d24579c42eb87d7fb51bb776f460
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-11-29 19:24:12 +0300

    remove unnecessary icons

---
+++ src/components/SidebarUnregistered.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+import LineIcon from "@/components/icons/IconLine.vue";
+import SettingsLogo from "@/components/icons/IconSettings.vue";
+import SchoolLogo from "@/components/icons/IconSchoolLogo.vue";
+</script>
+
+<template>
+  <nav
+      class=" mx-auto bg-white rounded-r-3xl flex flex-col justify-between items-center">
+    <div class="mx-auto mt-11">
+      <div>
+        <SchoolLogo  class="mx-auto"/>
+      </div>
+      <div class="mx-auto mt-7">
+        <LineIcon />
+      </div>
+    </div>
+    <div>
+      <div class="mb-9 ">
+        <SettingsLogo class="mx-auto"/>
+      </div>
+
+
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+
+</style>
