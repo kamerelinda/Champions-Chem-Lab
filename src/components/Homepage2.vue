847af7538604f3bd7351a7f45cd8e12859a4c1f6
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-11-29 19:21:24 +0300

    Insert registered sidebar version

--- src/components/Homepage2.vue
+++ src/components/Homepage2.vue
@@ -3,13 +3,12 @@ import TopNavbar from "@/components/TopNavbar.vue";
 import TrainingVideos from "@/components/TrainingVideos.vue";
 import JustForYou from "@/components/JustForYou.vue";
 import TrendingVideos from "@/components/TrendingVideos.vue";
-
-
-
+import SideBarNavigation from "@/components/SideBarNavigation.vue";
 </script>
 
 <template>
   <div class="w-full h-full relative">
+    <SideBarNavigation class="absolute z-50 w-24 shadow-2xl h-full"/>
     <TopNavbar/>
     <TrainingVideos/>
     <div class="p-[20px] bg-white w-[1095px] rounded-[5px] ml-[207px] absolute bottom-[5px]">
