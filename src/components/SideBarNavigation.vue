36117e568a30de7f245f3be2a145eddd342518c2
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-11-29 19:24:53 +0300

    add icons for registered sidebar

--- src/components/SideBarNavigation.vue
+++ src/components/SideBarNavigation.vue
@@ -1,44 +1,37 @@
 <script setup lang="ts">
-  import SchoolLogo from "./icons/IconSchoolLogo.vue"
   import SettingsLogo from "./icons/IconSettings.vue"
   import LineIcon from "./icons/IconLine.vue"
   import RegisteredNav from "@/components/RegisteredNav.vue";
   import IconLogout from "@/components/icons/IconLogout.vue";
   import IconChampionsLab from "@/components/icons/IconChampionsLab.vue";
 
-  import {useLoadHomeStore} from "@/stores/loadHomepage";
-  const loadHome = useLoadHomeStore()
-
-  import  {RouterLink} from "vue-router";
+  import {useSidebarOpen} from "@/stores/sidebarOpen";
+  const openBar = useSidebarOpen()
 
 </script>
 
 <template>
-    <nav class="w-24 bg-white rounded-r-3xl flex flex-col justify-between">
-      <div class="mx-auto mt-11">
+    <nav :class="openBar.isSidebarOpen ? 'w-64' : 'w-24'"
+        class=" mx-auto bg-white rounded-r-3xl flex flex-col justify-between items-center">
+      <div class=" mt-11">
         <div>
-          <RouterLink to="/">
-            <IconChampionsLab  v-if="loadHome.Home2" class="mx-auto"/>
-          <SchoolLogo v-else class="mx-auto"/>
-          </RouterLink>
+            <IconChampionsLab @click="openBar.toggleSidebar" class="mx-auto "/>
         </div>
-        <div class="mx-auto mt-7">
-          <LineIcon/>
+        <div class=" mt-7">
+          <LineIcon />
         </div>
-        <div>
-          <RegisteredNav v-show="loadHome.Home2"/>
+        <div >
+          <RegisteredNav />
         </div>
       </div>
-<div>
-  <div v-if="loadHome.Home2">
-    <SettingsLogo class="mx-auto"/>
-    <icon-logout class=" mx-auto mb-9 mt-11"/>
-  </div>
-    <div v-else class="mb-9 ">
-      <SettingsLogo class="mx-auto"/>
-    </div>
+      <div>
+        <div>
+          <SettingsLogo class="mx-auto"/>
+          <icon-logout class=" mx-auto mb-9 mt-11"/>
+        </div>
 
 
-</div>
+      </div>
     </nav>
+
 </template>
