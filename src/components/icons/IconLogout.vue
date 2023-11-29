ecadab933a8bd0432e4ff45d3a4471464f10945b
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-11-29 19:13:18 +0300

    Add labels to sidebar icons when sidebar is expanded

--- src/components/icons/IconLogout.vue
+++ src/components/icons/IconLogout.vue
@@ -1,5 +1,12 @@
 <template>
+  <div class="flex justify-center">
   <svg xmlns="http://www.w3.org/2000/svg" width="22" height="25" viewBox="0 0 22 25" fill="none">
     <path d="M15.3795 16.8926L18.9982 12.8926M18.9982 12.8926L15.3795 8.89258M18.9982 12.8926L6.33275 12.8926M11.7608 16.8926V17.8926C11.7608 19.5494 10.5457 20.8926 9.04677 20.8926H5.42807C3.92916 20.8926 2.71405 19.5494 2.71405 17.8926V7.89258C2.71405 6.23572 3.92916 4.89258 5.42807 4.89258H9.04677C10.5457 4.89258 11.7608 6.23572 11.7608 7.89258V8.89258" stroke="#FF5C5C" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
   </svg>
+  <span class="ml-4" v-if="openBar.isSidebarOpen">Logout</span>
+  </div>
 </template>
+<script setup lang="ts">
+import {useSidebarOpen} from "@/stores/sidebarOpen";
+const openBar = useSidebarOpen()
+</script>
--- src/components/icons/IconTrainingVideo.vue
+++ src/components/icons/IconTrainingVideo.vue
@@ -1,5 +1,12 @@
 <template>
+  <div class="flex justify-center">
   <svg xmlns="http://www.w3.org/2000/svg" width="26" height="27" viewBox="0 0 26 27" fill="none">
     <path d="M4 5L6 9H9L7 5H9L11 9H14L12 5H14L16 9H19L17 5H20C20.55 5 21.0208 5.19583 21.4125 5.5875C21.8042 5.97917 22 6.45 22 7V19C22 19.55 21.8042 20.0208 21.4125 20.4125C21.0208 20.8042 20.55 21 20 21H4C3.45 21 2.97917 20.8042 2.5875 20.4125C2.19583 20.0208 2 19.55 2 19V7C2 6.45 2.19583 5.97917 2.5875 5.5875C2.97917 5.19583 3.45 5 4 5ZM4 11V19H20V11H4Z" fill="#EBCE39"/>
   </svg>
+  <span class="ml-4" v-if="openBar.isSidebarOpen">Training Videos</span>
+  </div>
 </template>
+<script setup lang="ts">
+import {useSidebarOpen} from "@/stores/sidebarOpen";
+const openBar = useSidebarOpen()
+</script>
