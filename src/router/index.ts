2e200292fd88e6bed76925690e87075629b5d768
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-12-01 21:55:01 +0300

    add routing for login and signup

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,51 +1,68 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from "@/views/HomeView.vue";
-import VirtualGamesView from "@/views/VirtualGamesView.vue";
-import AssignmentsView from "@/views/AssignmentsView.vue";
-import CommunitiesView from "@/views/CommunitiesView.vue";
-import VideoLessonsView from "@/views/VideoLessonsView.vue";
-import PuzzleGamesView from "@/views/PuzzleGamesView.vue";
-import AdminRegistrationView from "@/views/AdminRegistrationView.vue";
-import LoginView from "@/views/LoginView.vue";
+import HomeView from "@/views/Home/HomeView.vue";
+import VirtualGamesView from "@/views/Nav Pages/VirtualGamesView.vue";
+import AssignmentsView from "@/views/Nav Pages/AssignmentsView.vue";
+import CommunitiesView from "@/views/Nav Pages/CommunitiesView.vue";
+import VideoLessonsView from "@/views/Nav Pages/VideoLessonsView.vue";
+import PuzzleGamesView from "@/views/Nav Pages/PuzzleGamesView.vue";
+import HomePageView from "@/views/Home/HomePageView.vue";
+import AdminLoginView from "@/views/Admin Login/AdminLoginView.vue";
+import AdminRegistrationView from "@/views/Admin SignUp/AdminRegistrationView.vue";
+import AdminActivateView from "@/views/Admin SignUp/AdminActivateView.vue";
+import AdminBuildClassView from "@/views/Admin SignUp/AdminBuildClassView.vue";
+import StudentLoginView from "@/views/Student Login/StudentLoginView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/register',
-      name: 'register',
+      path: '/signup',
+      name: 'admin signup',
       component: AdminRegistrationView
     },
     {
-      path: '/login',
-      name: 'login',
-      component: LoginView
+      path: '/signup/build_classroom',
+      name: 'build my class',
+      component: AdminBuildClassView
+    },
+    {
+      path: '/signup/activate_plan',
+      name: 'activation',
+      component: AdminActivateView
+    },
+    {
+      path: '/admin_login',
+      name: 'admin login',
+      component: AdminLoginView
+    },
+    {
+      path: '/student_login',
+      name: 'student login',
+      component: StudentLoginView
+    },
+    {
+      path: '/homepage',
+      name: 'registered home',
+      component: HomePageView
     },
     {
       path: '/',
       name: 'home1',
       component: HomeView
     },
+
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/3dgames',
+      path: '/3d_games',
       name: 'gaming',
       component: VirtualGamesView
     },
     {
-      path: '/puzzlegames',
+      path: '/puzzle_games',
       name: 'puzzles',
       component: PuzzleGamesView
     },
     {
-      path: '/videolessons',
+      path: '/video_lessons',
       name: 'training videos',
       component: VideoLessonsView
     },
