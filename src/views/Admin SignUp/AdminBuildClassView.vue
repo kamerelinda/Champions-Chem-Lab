e08bc90b1eed082e542cb3e7cb4cdd379286013b
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-12-01 21:57:27 +0300

    add new view for build class page

---
+++ src/views/Admin SignUp/AdminBuildClassView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+  import AdminBuildClassRoom from "@/components/Admin/Admin Signup/AdminBuildClassRoom.vue";
+</script>
+
+<template>
+  <AdminBuildClassRoom/>
+</template>
