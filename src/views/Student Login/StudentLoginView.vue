9dde20cdd3452e65b7c3e44a17d2e48f28d3060e
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-12-01 21:58:24 +0300

    add new view for student login page

---
+++ src/views/Student Login/StudentLoginView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  import StudentLogin from "@/components/Student/Student Login/StudentLogin.vue";
+</script>
+
+<template>
+  <StudentLogin/>
+</template>
+
+<style scoped>
+
+</style>
